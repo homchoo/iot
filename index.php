5c63eab49fe6350d1f4fd2370df5cc48596da4fc
homchoo <homchoo@gmail.com>
2021-09-26 11:31:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,61 @@
 <?php
+// $photo=$_GET["photo"];
+/*-------------line noti----------------------*/
+$line_api = 'https://notify-api.line.me/api/notify';
+    $access_token = "q3z1jYmFhn2A2Ee2fGiAdRLaP5PEbqvRtuFaYm/leVoFJ1JtZggY0xuMTjDMLsjj96Foc0dZY+l977JRm9ysL2vc/DjWrMqn7nz33FLPTI8oC5MaWBv6ODbMEP9oG3L/8O6KRekJxX1mSKqwlzF9cgdB04t89/1O/w1cDnyilFU=" ;
+
+    $message = 'test send photo';    //text max 1,000 charecter
+    $image_thumbnail_url = 'http://localhost/line/send-photo/photo-thumb.jpg';  // max size 240x240px JPEG
+    $image_fullsize_url = 'https://uts.ac.th/lex/gpa/picteacher/0777d5c17d4066b82ab86dff8a46af6f.jpg' //$photo; //max size 1024x1024px JPEG
+    $imageFile = 'copy/240.jpg';
+    $sticker_package_id = '';  // Package ID sticker
+    $sticker_id = '';    // ID sticker
+
+    $message_data = array(
+  'imageThumbnail' => $image_thumbnail_url,
+  'imageFullsize' => $image_fullsize_url,
+  'message' => $message,
+  'imageFile' => $imageFile,
+  'stickerPackageId' => $sticker_package_id,
+  'stickerId' => $sticker_id
+    );
+
+    $result = send_notify_message($line_api, $access_token, $message_data);
+
+echo '<pre>';
+     print_r($result);
+     echo '</pre>';
+/*-------------line noti----------------------*/
+
+
+
+function send_notify_message($line_api, $access_token, $message_data){
+   $headers = array('Method: POST', 'Content-type: multipart/form-data', 'Authorization: Bearer '.$access_token );
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $line_api);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $message_data);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   $result = curl_exec($ch);
+   // Check Error
+   if(curl_error($ch))
+   {
+      $return_array = array( 'status' => '000: send fail', 'message' => curl_error($ch) );
+   }
+   else
+   {
+      $return_array = json_decode($result, true);
+   }
+   curl_close($ch);
+return $return_array;
+}
+
+?>
 /*
+ 
+
 $token = "q3z1jYmFhn2A2Ee2fGiAdRLaP5PEbqvRtuFaYm/leVoFJ1JtZggY0xuMTjDMLsjj96Foc0dZY+l977JRm9ysL2vc/DjWrMqn7nz33FLPTI8oC5MaWBv6ODbMEP9oG3L/8O6KRekJxX1mSKqwlzF9cgdB04t89/1O/w1cDnyilFU=" ; // LINE Token
 //Message
 $mymessage = "เรื่อง: ทดสอบส่งข้อความ \n"; //Set new line with '\n'
@@ -33,8 +89,7 @@ $sticker_id = '34';    // ID sticker
   //Close connection
   curl_close( $chOne );
 
-?>
-*/
+?> 
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
 $ACCESS_TOKEN = 'q3z1jYmFhn2A2Ee2fGiAdRLaP5PEbqvRtuFaYm/leVoFJ1JtZggY0xuMTjDMLsjj96Foc0dZY+l977JRm9ysL2vc/DjWrMqn7nz33FLPTI8oC5MaWBv6ODbMEP9oG3L/8O6KRekJxX1mSKqwlzF9cgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
