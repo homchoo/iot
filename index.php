75d57faa671f7ff08715d9930910c02f3598c6d6
homchoo <homchoo@gmail.com>
2021-09-26 11:08:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
-<?php  
+ 
+<?php
+$token = "q3z1jYmFhn2A2Ee2fGiAdRLaP5PEbqvRtuFaYm/leVoFJ1JtZggY0xuMTjDMLsjj96Foc0dZY+l977JRm9ysL2vc/DjWrMqn7nz33FLPTI8oC5MaWBv6ODbMEP9oG3L/8O6KRekJxX1mSKqwlzF9cgdB04t89/1O/w1cDnyilFU=" ; // LINE Token
+//Message
+$mymessage = "เรื่อง: ทดสอบส่งข้อความ \n"; //Set new line with '\n'
+$mymessage .= "จาก: ข้อความจากแมว \n";
+$mymessage .= "รายละเอียด: แมวหิวแล้วจ้า";
+$imageFile = new CURLFILE('cat.jpg'); // Local Image file Path
+$sticker_package_id = '2';  // Package ID sticker
+$sticker_id = '34';    // ID sticker
+  $data = array (
+    'message' => $mymessage,
+    'imageFile' => $imageFile,
+    'stickerPackageId' => $sticker_package_id,
+    'stickerId' => $sticker_id
+  );
+  $chOne = curl_init();
+  curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+  curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt( $chOne, CURLOPT_POST, 1);
+  curl_setopt( $chOne, CURLOPT_POSTFIELDS, $data);
+  curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1);
+  $headers = array( 'Method: POST', 'Content-type: multipart/form-data', 'Authorization: Bearer '.$token, );
+  curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec( $chOne );
+  //Check error
+  if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); }
+  else { $result_ = json_decode($result, true);
+  echo "status : ".$result_['status']; echo "message : ". $result_['message']; 
+  }
+  //Close connection
+  curl_close( $chOne );
+
+?>
+/*
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
 $ACCESS_TOKEN = 'q3z1jYmFhn2A2Ee2fGiAdRLaP5PEbqvRtuFaYm/leVoFJ1JtZggY0xuMTjDMLsjj96Foc0dZY+l977JRm9ysL2vc/DjWrMqn7nz33FLPTI8oC5MaWBv6ODbMEP9oG3L/8O6KRekJxX1mSKqwlzF9cgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
@@ -61,4 +97,4 @@ function send_reply_message($url, $post_header, $post_body)
 }
 
 ?>
- 
+*/ 
