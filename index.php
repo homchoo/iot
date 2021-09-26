fc94a1e765aa777bec6ece523629c10f32e72d5c
homchoo <homchoo@gmail.com>
2021-09-26 12:00:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ if ( sizeof($request_array['events']) > 0 )
    $data = [
     'replyToken' => $reply_token,
     'messages' => [['type' => 'text', 'text' => $reply_message]],
-     'image' => [['type' => 'image', 'originalContentUrl' => $image_fullsize_url,'previewImageUrl' => $image_thumbnail_url]]
+     'images' => [['type' => 'image', 'originalContentUrl' => $image_fullsize_url,'previewImageUrl' => $image_thumbnail_url]]
    ];
     
 
